081fb4e8550ffdc6f907253f152b0e29576ea64e
Nikita Polozov <npolozov0@gmail.com>
2022-09-08 12:22:15 +0300

    !

--- JS/mobile-menu.js
+++ JS/mobile-menu.js
@@ -1,29 +1,17 @@
 (() => {
-  const mobileMenu = document.querySelector(".js-menu-container");
-  const openMenuBtn = document.querySelector(".js-open-menu");
-  const closeMenuBtn = document.querySelector(".js-close-menu");
+  const menuBtnRef = document.querySelectorAll("[data-menu-button]");
+  const mainButtonRef = menuBtnRef[0];
+  const mobileMenuRef = document.querySelector("[data-menu]");
 
-  const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute("aria-expanded") === "true" || false;
-    openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
-    mobileMenu.classList.toggle("is-open");
+  menuBtnRef.forEach((el) =>
+    el.addEventListener("click", () => {
+      const expanded =
+        mainButtonRef.getAttribute("aria-expanded") === "true" || false;
 
-    // const scrollLockMethod = !isMenuOpen
-    //   ? "disableBodyScroll"
-    //   : "enableBodyScroll";
-    // bodyScrollLock[scrollLockMethod](document.body);
-  };
+      mainButtonRef.classList.toggle("is-open");
+      mainButtonRef.setAttribute("aria-expanded", !expanded);
 
-  openMenuBtn.addEventListener("click", toggleMenu);
-  closeMenuBtn.addEventListener("click", toggleMenu);
-
-  // Закрываем мобильное меню на более широких экранах
-  // в случае изменения ориентации устройства.
-  window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove("is-open");
-    openMenuBtn.setAttribute("aria-expanded", false);
-    // bodyScrollLock.enableBodyScroll(document.body);
-  });
+      mobileMenuRef.classList.toggle("is-open");
+    })
+  );
 })();
